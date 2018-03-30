360a2e0bcdd08af49d1a310481a31ab937045858
Michael Londeen <jollymike@gmail.com>
2018-03-30 00:03:55 -0500

    Target all sites. Change bg and text color.

--- borderify.js
+++ borderify.js
@@ -1,1 +1,7 @@
-document.body.style.border = "5px solid red";
+var db = document.body.getElementsByTagName('*');
+
+for (var i=0; i < db.length; i++) {
+    // db[i].setAttribute("class", "publish_1");
+    db[i].style.background = "#222233";
+    db[i].style.color = "#BFE3CA";
+}
