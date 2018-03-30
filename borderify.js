60bea39fb291a07820c5b851540f2539ff599617
Michael Londeen <jollymike@gmail.com>
2018-03-30 00:25:14 -0500

    change targeting

--- borderify.js
+++ borderify.js
@@ -1,7 +1,18 @@
-var db = document.body.getElementsByTagName('*');
+var tags = ['body','div','main','article']
+for (var j=0; j < tags.length; j++){
+  var db = document.body.getElementsByTagName(tags[j]);
 
-for (var i=0; i < db.length; i++) {
-    // db[i].setAttribute("class", "publish_1");
-    db[i].style.background = "#222233";
-    db[i].style.color = "#BFE3CA";
+  for (var i=0; i < db.length; i++) {
+      db[i].style.background = "#222233";
+      // db[i].style.color = "#BFE3CA";
+  }
+}
+
+var tags = ['a','i','p','h1','h2','h3','h4','h5','span']
+for (var j=0; j < tags.length; j++){
+  var db = document.body.getElementsByTagName(tags[j]);
+
+  for (var i=0; i < db.length; i++) {
+      db[i].style.color = "#BFE3CA";
+  }
 }
